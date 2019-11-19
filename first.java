24a128eb703171e8b4e5862485bb64943190254f
simplyesu <simplyesu@gmail.com>
2019-11-19 09:08:11 +0530

    Update first.java

--- first.java
+++ first.java
@@ -1,7 +1,7 @@
 /** 
  * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
  */
-public class build {
+public class first {
   public static void main(String[] args) {
     System.out.println("This is my first build project");
   }
